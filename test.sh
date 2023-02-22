053d0139d5f550e189adcb9ee143cf6cb76d834b
Alam <125980952+cloudalam311@users.noreply.github.com>
2023-02-22 20:18:15 +0530

    delete test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-app="httpd wget unzip"
-##Installing Dependencies
-echo "$app Installing Packages."
-#echo "#########################################################"
-sudo yum install $app -y 
-
-
